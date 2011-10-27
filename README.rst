05342db782b83c3df2c06c61540cb52dad659cbb
exupero <eric.shull@gmail.com>
2011-10-27 10:54:52 -0400

    Added documentation, showing feature files as links.

---
+++ README.rst
@@ -0,0 +1,17 @@
+Trepang
+=======
+
+A web interface for Cucumber_. To use, navigate to a directory that contains Cucumber tests
+and run ``trepang``.
+
+.. _Cucumber: http://cukes.info/
+
+
+Dependencies
+------------
+
+* Sinatra_
+* Haml_
+
+.. _Sinatra: http://www.sinatrarb.com/
+.. _Haml: http://haml-lang.com/
--- server.rb
+++ server.rb
@@ -2,5 +2,15 @@ require 'haml'
 require 'sinatra'
 
 get '/' do
-  haml :index
+  features = Dir.glob('features/*').collect do |filename|
+    open(filename) do |file|
+      match = /^Feature: (.*)$/.match file.readline
+      match.captures[0]
+    end
+  end
+
+  haml :index, :locals => {
+    :pwd => Dir.pwd,
+    :features => features
+  }
 end
