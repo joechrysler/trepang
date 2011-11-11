7c2b564de32d94871c13c5e41846565bd4fa2200
Joe Chysler <mail@joechrysler.com>
2011-11-11 13:15:06 -0500

    linkify the step_file paths

--- public/js/trepang.js
+++ public/js/trepang.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
   console.log('here');
+  var step_file = $('.step_file span, .scenario_file');
+  step_file.html("<a href='" + step_file.html() + "'>" + step_file.html() + "</a>");
 });
