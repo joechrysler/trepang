7df64115caf57a47a8924d311f4667fe0ab13da5
Joe Chysler <mail@joechrysler.com>
2011-11-21 14:24:36 -0500

    add a file reading route

--- server.rb
+++ server.rb
@@ -4,6 +4,8 @@ require 'erb'
 require 'haml'
 require 'sinatra'
 
+require 'config'
+
 
 set :static, true
 set :public_folder, File.dirname(__FILE__) + '/public'
@@ -13,4 +15,8 @@ get '/' do
   html = `cucumber -f html`
   html = html.gsub /<Response>/, '&lt;Response&gt;'
   html.gsub /<\/body>/, '<link rel="stylesheet" type="text/css" href="/css/trepang.css" /><script type="text/javascript" src="/js/trepang.js"></script></body>'
+end
+
+get '/file/:path' do |path|
+  File.read(path)
 end
